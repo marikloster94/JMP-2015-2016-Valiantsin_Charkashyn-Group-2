f8612a4346dbaeb3c16b1e89cd289d50af3bd6a8
marikloster94 <mari.kloster94@gmail.com>
2015-11-30 11:50:06 +0300

    final refactoring

--- LabyrinthGame/src/main/java/com/epam/game/main/Runner.java
+++ LabyrinthGame/src/main/java/com/epam/game/main/Runner.java
@@ -13,21 +13,16 @@ public class Runner {
 	public static void main(String[] args) {
 		try {
 			PlayGame player = new PlayGame();
-			System.out.println("Loading labyrinth");
 			Labyrinth labyrinth = player.loadLabyrinth();
 			System.out.println("Labyrinth loaded");
-			System.out.println("Checking labyrinth");
 			player.chekLabyrinth(labyrinth);
-			System.out.println("Labyrinth checked");
-			System.out.println("Loading behaviors");
+			System.out.println("Labyrinth checked");;
 			List<Behavior> behaviors = player.loadDuckBehaviors();
 			System.out.println("Behaviors loaded");
-			System.out.println("Loading ducks");
 			List<Duck> ducks = player.loadDucks(behaviors);
 			System.out.println("Ducks loaded");
-			System.out.println("Choose enterance for duck");
 			player.chooseStartCoordinates(ducks, labyrinth);
-			System.out.println("Enterance choosed");
+			System.out.println("Enterance for duck choosed");
 			for(Duck duck:ducks){
 				labyrinth = player.loadLabyrinth();
 				player.playGame(labyrinth, duck, player.loadPassingAlgorithms());
